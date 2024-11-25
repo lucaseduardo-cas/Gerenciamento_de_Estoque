d33fbae4c7f8a8e2618c6a2bd9417eff10671b5e
Lucas <lucas.alfra@gmail.com>
2024-11-25 20:25:59 -0300

    Menu de Cadastro atualizado
    Menu de Cadastro dentro do laço de repetição

--- estoque.c
+++ estoque.c
@@ -10,18 +10,48 @@ struct cadastro {
 
 int main (void) {
 	setlocale(LC_ALL, "Portuguese");
-
-    int opcao;
 	
-    printf("[1] Adicionar produto\n");
-	printf("[2] Consultar estoque\n");
-	printf("[3] Vender produto\n");
-	printf("[4] Sair\n");
-	printf("Opção: ");
-	scanf("%d", &opcao);
-	printf("\n");
-
+	struct cadastro Cadastro;
 	
+	char nome[20];
+	int opcao, qtd;
+	float preco;
+	int i;
+	
+	do {
+		printf("[1] Adicionar produto\n");
+		printf("[2] Consultar estoque\n");
+		printf("[3] Vender produto\n");
+		printf("[4] Sair\n");
+		printf("Opção: ");
+		scanf("%d", &opcao);
+		printf("\n");
+	
+		switch(opcao) {
+			case 1:
+				printf("Nome: ");
+				scanf("%s", &Cadastro.nome);
+				printf("Quantidade: ");
+				scanf("%d", &Cadastro.qtd);
+				printf("Preço: R$ ");
+				scanf("%d", &Cadastro.preco);
+				printf("\n");
+				break;
+				
+			case 2: 
+				break;
+				
+			case 3: 
+				break;
+				
+			case 4:
+				break;	
+			
+			default: 
+				printf("Valor inválido!");
+				break; 
+		}
+	} while(1);
 	
 	system("pause");
 	return 0;
